2410afc8eec762c5d4c386961746bf3abf0b3eaf
hyochan <dooboolab@gmail.com>
2018-12-11 22:35:24 +0900

    Add join slack link.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.joinSlack = functions.https.onRequest((request, response) => {
+    response.redirect('https://join.slack.com/t/dooboolab/shared_invite/enQtNTAwODgxNDExNTg2LTMzYTNlZmE2M2FlNGNlYWU1NzcwZTY0NTI1NWMyYmU5NWViNzQxNjhiYzE0MjdjNzg1MjhlM2VhM2U4YWRhZjM');
+});
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,6 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const joinSlack = functions.https.onRequest((request, response) => {
+ response.redirect('https://join.slack.com/t/dooboolab/shared_invite/enQtNTAwODgxNDExNTg2LTMzYTNlZmE2M2FlNGNlYWU1NzcwZTY0NTI1NWMyYmU5NWViNzQxNjhiYzE0MjdjNzg1MjhlM2VhM2U4YWRhZjM');
+});
