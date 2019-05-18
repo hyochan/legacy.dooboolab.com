75a2858f98a2776a2dbddd915f1e6cede7af2a33
hyochan <dooboolab@gmail.com>
2019-05-19 00:05:55 +0900

    Fix eslint

--- test/jestSetup.ts
+++ test/jestSetup.ts
@@ -4,7 +4,7 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 console.error = (message) => {
-    if (!/(React.createElement: type should not be null)/.test(message)) {
-        throw new Error(message);
-    }
+  if (!/(React.createElement: type should not be null)/.test(message)) {
+    throw new Error(message);
+  }
 };
