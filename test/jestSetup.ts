5f764a65b4096ab7d6f1051cf688a35e990ce1a7
hyochan <dooboolab@gmail.com>
2019-05-19 17:52:12 +0900

    Fix jest

--- test/jestSetup.ts
+++ test/jestSetup.ts
@@ -1,7 +1,7 @@
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import * as Enzyme from 'enzyme';
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
 
-// configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new ReactSixteenAdapter() });
 
 // console.error = (message) => {
 //   if (!/(React.createElement: type should not be null)/.test(message)) {
