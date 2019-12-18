5e3922ba040ef5b13c7c9d2e0e39fe82df9784bd
hyochan <dooboolab@gmail.com>
2019-12-19 02:38:12 +0900

    Update slack urls

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -4,7 +4,10 @@ const functions = require("firebase-functions");
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-exports.joinSlack = functions.https.onRequest((request, response) => {
-    response.redirect('https://join.slack.com/t/dooboolab/shared_invite/enQtNTAwODgxNDExNTg2LTMzYTNlZmE2M2FlNGNlYWU1NzcwZTY0NTI1NWMyYmU5NWViNzQxNjhiYzE0MjdjNzg1MjhlM2VhM2U4YWRhZjM');
+exports.joinSlack = functions.https.onRequest((_, response) => {
+    response.redirect('https://join.slack.com/t/dooboolab/shared_invite/enQtNTAwODgxNDExNTg2LTkzNTA3ZmJmZDM4YzQ3ZWEyNTI3MjdkZjJmYjNhNjlmY2UzYjMzZTJjMjVkMmIzM2M1Y2QyZjlkN2NkZjhmZWE');
+});
+exports.joinRNSeoul = functions.https.onRequest((_, response) => {
+    response.redirect('https://join.slack.com/t/reactnativeseoul/shared_invite/enQtMjY4ODI2OTgzNzE0LWU4MWUzNjBkMDE4YTBjNjBmNmVhNjIwOWQzZmQxMTRjZGVhMzA2YWZhNWQ1ZWQ5MGJkODhlNGRlYmQzNzkzOWQ');
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,14 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const joinSlack = functions.https.onRequest((request, response) => {
- response.redirect('https://join.slack.com/t/dooboolab/shared_invite/enQtNTAwODgxNDExNTg2LTkzNTA3ZmJmZDM4YzQ3ZWEyNTI3MjdkZjJmYjNhNjlmY2UzYjMzZTJjMjVkMmIzM2M1Y2QyZjlkN2NkZjhmZWE');
+export const joinSlack = functions.https.onRequest((_, response) => {
+  response.redirect(
+    'https://join.slack.com/t/dooboolab/shared_invite/enQtNTAwODgxNDExNTg2LTkzNTA3ZmJmZDM4YzQ3ZWEyNTI3MjdkZjJmYjNhNjlmY2UzYjMzZTJjMjVkMmIzM2M1Y2QyZjlkN2NkZjhmZWE',
+  );
+});
+
+export const joinRNSeoul = functions.https.onRequest((_, response) => {
+  response.redirect(
+    'https://join.slack.com/t/reactnativeseoul/shared_invite/enQtMjY4ODI2OTgzNzE0LWU4MWUzNjBkMDE4YTBjNjBmNmVhNjIwOWQzZmQxMTRjZGVhMzA2YWZhNWQ1ZWQ5MGJkODhlNGRlYmQzNzkzOWQ',
+  );
 });
