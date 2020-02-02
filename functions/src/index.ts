e08e4e5f7ec0453a9c26cf553c28c3d87acb4d20
hyochan <dooboolab@gmail.com>
2020-02-02 15:09:56 +0900

    Add graphqlSeoul invitation link

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,3 +14,9 @@ export const joinRNSeoul = functions.https.onRequest((_, response) => {
     'https://join.slack.com/t/reactnativeseoul/shared_invite/enQtMjY4ODI2OTgzNzE0LWU4MWUzNjBkMDE4YTBjNjBmNmVhNjIwOWQzZmQxMTRjZGVhMzA2YWZhNWQ1ZWQ5MGJkODhlNGRlYmQzNzkzOWQ',
   );
 });
+
+export const joinGraphQLSeoul = functions.https.onRequest((_, response) => {
+  response.redirect(
+    'https://join.slack.com/t/graphql-seoul/shared_invite/enQtOTMyNzc4NzYxMjg1LTMwNzg3ODFiY2I2ODlkMWFiZTU0NDg5Zjg0MDRkMGY3MGExZTAwZTIxNmE1YzJkNGJhMmU5ZGYzYTY0N2EzNDY',
+  );
+});
